932c91884a913f5f775ee611308cc1e337dac82a
maxime7513 <makitoo@outlook.com>
2021-01-16 23:00:20 +0100

    commit page inscription

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,13 @@
 console.log('MON CODE JS EST CHARGE');
 
+// SLIDER BACKGROUND PAGE ACCUEIL
 let images = [
     'assets/img/slideshow1.jpg',        // INDICE 0
     'assets/img/slideshow2.jpg',        // INDICE 1
     'assets/img/slideshow3.jpg',        // INDICE 2
-    'assets/img/slideshow4.jpg',        // INDICE 2
-    'assets/img/slideshow5.jpg',         // INDICE 2
-    'assets/img/slideshow6.jpg'         // INDICE 2
+    'assets/img/slideshow4.jpg',        // INDICE 3
+    'assets/img/slideshow5.jpg',        // INDICE 4
+    'assets/img/slideshow6.jpg'         // INDICE 5
 ];
 let slideshow = document.querySelector('.slideshow');
 srcSuivant = images[compteur];
@@ -16,15 +17,36 @@ var compteur = 0;
         compteur = (compteur +1) % images.length;   // ASTUCE QUI VA FAIRE BOUCLER A ZERO SI ON DEBORDE
         srcSuivant = images[compteur];
         slideshow.style.backgroundImage = "url('" + srcSuivant + "')";
-
     },
-    2000);  // 2s
+    3000);  // 3s
 
     var scrollTop = $(".scrollTop");
 
+// REMONTER EN HAUT DE PAGE AVEC FLECHE PAGE ACCUEIL
 $(scrollTop).click(function() {
   $('html, body').animate({
     scrollTop: 0
   }, 1400);
   return false;
 });
+
+// CHANGER LE HEADER POUR MOBILE
+let headerPc= document.querySelector('.header-pc');
+let headerMobile= document.querySelector('.header-mobile');
+  if (window.matchMedia("(max-width: 642px)").matches){ //642px
+      headerPc.classList.remove("d-flex");
+      headerPc.classList.add("d-none");
+      headerMobile.classList.remove("d-none");
+}
+
+// CHANGER SYMBOLE MENU HAMBURGER
+let buttonHamburger = document.querySelector('.navbar-toggle');
+let iconeHamburger1 = document.querySelector('.fa-bars');
+let iconeHamburger2 = document.querySelector('.fa-times-circle');
+
+buttonHamburger.addEventListener('click', function () {
+    iconeHamburger1.classList.toggle("d-none");
+    iconeHamburger2.classList.toggle("d-none");
+});
+
+
