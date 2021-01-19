f0b49a743d7503b4eb9e067c6faadf8342753ca2
Joseph <josephifergan1@gmail.com>
2021-01-19 12:00:29 +0100

    crud tables

---
+++ src/Form/MeetingType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Meeting;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeetingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('id_user')
+            ->add('categorie')
+            ->add('sousCategorie')
+            ->add('userMax')
+            ->add('date')
+            ->add('heure')
+            ->add('adresse')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Meeting::class,
+        ]);
+    }
+}
