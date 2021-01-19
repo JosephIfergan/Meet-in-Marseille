8558f7c815794e5d0f768de4e25d3a3b5b2e0ce1
maxime <maxbln7513@gmail.com>
2021-01-19 20:15:17 +0100

    commit max

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -62,3 +62,4 @@ titreInscription.classList.remove('opacity-0');
 
 
 
+
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,17 +6,49 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
-            ->add('password')
-            ->add('sexe')
-            ->add('age')
+            ->add('pseudo',TextType::class, [
+                // 'label' => 'Votre prénom : ',
+                'attr' => [
+                    'placeholder' => 'choisir un pseudo'
+                ]
+            ])
+            ->add('email',EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Saisissez votre email'
+                ]
+            ])
+            ->add('password',PasswordType::class, [
+                'attr' => [
+                    'placeholder' => 'Mot de passe'
+                ]
+            ])
+            ->add('sexe', ChoiceType::class, [
+                'choices' => [
+                    'homme' => 'homme',
+                    'femme' => 'femme',
+                ],
+                'expanded' => true,
+                'attr' => [
+                    'class' => 'd-flex flex-row flex-wrap justify-content-center align-self-center'
+                ]
+            ])
+            ->add('age',IntegerType::class, [
+                'attr' => [
+                    'placeholder' => 'Age'
+                ]
+            ])
         ;
     }
 
