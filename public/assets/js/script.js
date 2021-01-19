559b0f3b33ffc863f3151a76aa9a198c69738832
Joseph <josephifergan1@gmail.com>
2021-01-19 22:17:36 +0100

    etapes ok

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -30,23 +30,11 @@ $(scrollTop).click(function() {
   return false;
 });
 
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".etape1").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
 
+// ANIMATION ETAPES 
+AOS.init({
+  duration: 1200,
+})
 
 
 
